7813a383c5e35219f43011a2ac418285677cc9be
Dominick Ruggero <druggero7@gmail.com>
2023-11-12 04:35:20 -0500

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,8 @@ const App = () => {
       <section>Portfolio2</section>
       <section>Portfolio3</section>
       <section>Contact</section>
+      {/*<Test />
+      <Test />*/}
     </div>
   )
 }
